bbc186043f2e386754973785fde43b96025421bf
SWi74 <dum66a@gmail.com>
2020-10-20 16:31:44 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const cars = {
+    "ford": {"color": "red", "maxSpeed": "200mph" },
+    "bmw": {"color": "blue", "maxSpeed": "210mph"}
+}
+
+
+console.log( cars.ford.color );
+console.log( cars.bmw.color );
