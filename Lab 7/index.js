0534ea8d93d4f04f123a390290bff028514ac67b
SWi74 <dum66a@gmail.com>
2020-11-10 19:26:53 +0200

    Undefined

--- Lab 7/index.js
+++ Lab 7/index.js
@@ -4,25 +4,19 @@ function Person(name, age) {
     this.greet = () => {console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old.`)}
 }
 
-const hans = new Person("Hans", 42)
-
 function Developer(name, age, skillset = []) {
-    this.name = name
-    this.age = age
+    Person.call(this, name, age)
     this.skillset = skillset
-    Object.create(Person, )
 }
 
-const chris = new Developer("Christian", 90, ["Java", "Elixir", "Cow"])
-
 function Manager(name, age, managees = []) {
-    Object.create(Person.prototype, {
-        
-    })
-    this.name = name
-    this.age = age
+    Person.call(this, name, age)
     this.managees = managees
 }
 
+
+const hans = new Person("Hans", 42)
+const chris = new Developer("Christian", 90, ["Java", "Elixir", "Cow"])
 const joro = new Manager("Jorkata", 19, ["Ruja", "Kamata", "Kiro"])
 
+console.log(joro.greet())
