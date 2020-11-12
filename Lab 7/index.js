6d3db3102ee7d1e61cc26e7c2978755057389ec6
SWi74 <dum66a@gmail.com>
2020-11-12 09:35:21 +0200

    Formatting list from array

--- Lab 7/index.js
+++ Lab 7/index.js
@@ -7,11 +7,17 @@ function Person(name, age) {
 function Developer(name, age, skillset = []) {
     Person.call(this, name, age)
     this.skillset = skillset
+    // How to put space after commas in list?
+    let formattedSkillset = skillset.join(", ")
+    this.greet = () => {console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old. I know ${formattedSkillset}`)}
 }
 
 function Manager(name, age, managees = []) {
     Person.call(this, name, age)
     this.managees = managees
+    // How to put space after commas in list?
+    let formattedManagees = managees.join(", ")
+    this.greet = () => {console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old. I manage ${formattedManagees}`)}
 }
 
 
@@ -19,4 +25,6 @@ const hans = new Person("Hans", 42)
 const chris = new Developer("Christian", 90, ["Java", "Elixir", "Cow"])
 const joro = new Manager("Jorkata", 19, ["Ruja", "Kamata", "Kiro"])
 
+hans.greet()
+chris.greet()
 joro.greet()
