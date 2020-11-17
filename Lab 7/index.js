b70409fd62a7ee7ce5f129a0930c5dd1dccbe2a0
SWi74 <dum66a@gmail.com>
2020-11-17 18:20:01 +0200

    Fixed Lab7

--- Lab 7/index.js
+++ Lab 7/index.js
@@ -1,25 +1,26 @@
 function Person(name, age) {
     this.name = name
     this.age = age
-    this.greet = () => {console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old.`)}
+}
+Person.prototype.greet = function () {
+    console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old.`)
 }
 
 function Developer(name, age, skillset = []) {
     Person.call(this, name, age)
     this.skillset = skillset
-    // How to put space after commas in list?
-    let formattedSkillset = skillset.join(", ")
-    this.greet = () => {console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old. I know ${formattedSkillset}`)}
+}
+Developer.prototype.greet = function () {
+    console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old. I know ${this.skillset.join(", ")}`)
 }
 
 function Manager(name, age, managees = []) {
     Person.call(this, name, age)
     this.managees = managees
-    // How to put space after commas in list?
-    let formattedManagees = managees.join(", ")
-    this.greet = () => {console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old. I manage ${formattedManagees}`)}
 }
-
+Manager.prototype.greet = function () {
+    console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old. I manage ${this.managees.join(", ")}`)
+}
 
 const hans = new Person("Hans", 42)
 const chris = new Developer("Christian", 90, ["Java", "Elixir", "Cow"])
